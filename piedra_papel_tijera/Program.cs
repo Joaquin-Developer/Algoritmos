ea2fc2a0772314bd69847b0c68dcbe8f68942751
Joaquin-Parrilla <joaquin.p.olivera@gmail.com>
2021-06-13 14:16:11 -0300

    add c# console program

---
+++ piedra_papel_tijera/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace piedra_papel_tijera
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese su nombre:");
+            string name = Console.ReadLine();
+            Console.WriteLine($"Nombre ingresado: { name }");
+        }
+    }
+
+    class Game
+    {
+
+    }
+
+}
